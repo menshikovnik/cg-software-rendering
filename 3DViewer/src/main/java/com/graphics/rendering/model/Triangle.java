2aaf08688760a0cbb33fade914e06f594631c411
Shao-Lin <baulin2005@bk.ru>
2023-11-26 13:30:02 +0300

    Добавил триангуляцию

--- 3DViewer/src/main/java/com/graphics/rendering/model/Triangle.java
+++ 3DViewer/src/main/java/com/graphics/rendering/model/Triangle.java
@@ -1,13 +1,14 @@
 package com.graphics.rendering.model;
 
+import com.graphics.rendering.math.Vector3f;
 import javafx.scene.Node;
 
-import java.awt.geom.Point2D;
-
 public class Triangle extends Node {
-    Point2D.Double p1, p2, p3;
+    Vector3f p1;
+    Vector3f p2;
+    Vector3f p3;
 
-    public Triangle(Point2D.Double p1, Point2D.Double p2, Point2D.Double p3) {
+    public Triangle(Vector3f p1, Vector3f p2, Vector3f p3) {
         this.p1 = p1;
         this.p2 = p2;
         this.p3 = p3;
