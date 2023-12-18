0f0359ebc6d85b55f7a9497cb6d8c8a8f6c3bf66
nikmenshikov <nikmeshikov1@gmail.com>
2023-12-19 00:26:18 +0300

    удалил метод рисования осей из render engine

--- 3DViewer/src/main/java/com/graphics/rendering/render_engine/RenderEngine.java
+++ 3DViewer/src/main/java/com/graphics/rendering/render_engine/RenderEngine.java
@@ -4,8 +4,6 @@ import com.graphics.rendering.math.matrix.Matrix4D;
 import com.graphics.rendering.math.vector.Vector3D;
 import com.graphics.rendering.model.Model;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 
 import javax.vecmath.Point2f;
 import java.util.ArrayList;
@@ -58,38 +56,5 @@ public class RenderEngine {
                             resultPoints.get(0).y);
             }
         }
-    }
-
-    private static void renderAxes(final GraphicsContext graphicsContext, final Matrix4D projectionViewModelMatrix, final int width, final int height) {
-        // Ось X (красная)
-        Line xAxis = new Line(0, 10000, 0, 0);
-        xAxis.setStroke(Color.RED);
-        drawLine(graphicsContext, projectionViewModelMatrix, xAxis, width, height);
-
-        // Ось Y (зеленая)
-        Line yAxis = new Line(0, 0, 1000000, 0);
-        yAxis.setStroke(Color.GREEN);
-        drawLine(graphicsContext, projectionViewModelMatrix, yAxis, width, height);
-
-        // Ось Z (синяя)
-        Line zAxis = new Line(0, 0, 0, 100000);
-        zAxis.setStroke(Color.BLUE);
-        zAxis.setTranslateX(100);
-        drawLine(graphicsContext, projectionViewModelMatrix, zAxis, width, height);
-    }
-
-    private static void drawLine(final GraphicsContext graphicsContext, final Matrix4D projectionViewModelMatrix, final Line line, final int width, final int height) {
-        Vector3D start = new Vector3D((float) line.getStartX(), (float) line.getStartY(), 0);
-        Vector3D end = new Vector3D((float) line.getEndX(), (float) line.getEndY(), 0);
-
-        Point2f startPoint = vertexToPoint(projectionViewModelMatrix.multiplyVectorDivW(start), width, height);
-        Point2f endPoint = vertexToPoint(projectionViewModelMatrix.multiplyVectorDivW(end), width, height);
-
-        line.setStartX(startPoint.x);
-        line.setStartY(startPoint.y);
-        line.setEndX(endPoint.x);
-        line.setEndY(endPoint.y);
-
-        graphicsContext.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
     }
 }
